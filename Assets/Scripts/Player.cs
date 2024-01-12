7c2c25999983988d4d4e877696b7a79643cc334f
tiephoangdev <tiephoang.dev@gmail.com>
2024-01-12 14:17:51 +0700

    player move

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -15,9 +15,30 @@ public class Player : MonoBehaviour
     void Update()
     {
         var direction = input.GetDirection();
-        transform.position += direction * moveSpeed * Time.deltaTime;
-        transform.forward = Vector3.Slerp(transform.forward, direction, rotateSpeed * Time.deltaTime);
+        var coliider = GetComponent<BoxCollider>();
+
+        //walking
         isWalking = !direction.Equals(Vector3.zero);
+        transform.forward = Vector3.Slerp(transform.forward, direction, rotateSpeed * Time.deltaTime);
+
+        //move
+        var moveDistance = moveSpeed * Time.deltaTime;
+        var playerRadius = coliider.size.x / 2;
+        var playerHeight = transform.position + Vector3.up * coliider.size.x;
+        var directions = new[] {
+            direction,
+            new Vector3(direction.x, 0, 0).normalized,
+            new Vector3(0, 0, direction.z).normalized,
+        };
+        foreach (var item in directions)
+        {
+            if (isWalking && !Physics.CapsuleCast(transform.position, playerHeight, playerRadius, item, moveDistance))
+            {
+                transform.position += item * moveDistance;
+                break;
+            }
+        }
+
     }
 
 
