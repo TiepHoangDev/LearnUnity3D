ea49a8fc04bd9b109177583a30b2c9cde62da42e
tiephoangdev <tiephoang.dev@gmail.com>
2024-01-12 12:58:34 +0700

    Player animator

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,7 +8,7 @@ public class Player : MonoBehaviour
 {
     [SerializeField] GameInput input;
     [SerializeField] float moveSpeed = 7F;
-    [SerializeField] float rotateSpeed = 5F;
+    [SerializeField] float rotateSpeed = 10F;
     private bool isWalking;
 
     // Update is called once per frame
